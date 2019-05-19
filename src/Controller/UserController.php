89080830f40cbbc6bce63b6c80f06a3db26d104d
Unknown <clara.tiare@live.fr>
2019-05-19 21:07:24 +0200

    début design front page login et register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,5 +23,40 @@ class UserController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/profile", name="show_profile")
+     */
+    public function profile()
+    {
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+
+        return $this->render('user/profile.html.twig', [
+                'user' => $user,
+            ]);
+    }
+
+    /**
+     * @Route("/profile/edit", name="edit_profile", methods={"GET","POST"})
+     */
+    public function edit(Request $request): Response
+    {
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+        $user->setEmail($user->getEmail());
+        $user->setPassword($user->getPassword());
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('show_profile');
+        }
+
+        return $this->render('user/edit_profile.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
 }
